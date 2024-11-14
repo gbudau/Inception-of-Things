e607ce84672b0fb5caadeb741a953c21e0ec10ac
gbudau <54190400+gbudau@users.noreply.github.com>
2024-11-14 09:11:36 +0100

    Port forward the argocd server

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -3,3 +3,5 @@
 k3d cluster create develop
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+echo 'ArgoCD password:' $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
