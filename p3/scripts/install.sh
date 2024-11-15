388c2f8d5b06c182523685d59cde9b75edf95eeb
gbudau <54190400+gbudau@users.noreply.github.com>
2024-11-15 08:06:29 +0100

    Add ArgoCD application

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,7 +1,21 @@
 #!/bin/env bash
 
+# Install Docker
+curl -fsSL https://get.docker.com | sh -
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install k3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# Install ArgoCD
 k3d cluster create develop
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -f ../confs/application.yaml
 kubectl port-forward svc/argocd-server -n argocd 8080:443
 echo 'ArgoCD password:' $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
