f06d9206b0756f056e6608a93baf1de220e1881d
gbudau <54190400+gbudau@users.noreply.github.com>
2024-11-15 08:28:17 +0100

    Wait for ArgoCD to be up

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -17,5 +17,6 @@ k3d cluster create develop
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -f ../confs/application.yaml
+kubectl wait --for=condition=Ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=600s
 kubectl port-forward svc/argocd-server -n argocd 8080:443
 echo 'ArgoCD password:' $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
