bd3a7d6f541c287bd5decc8294a0110d422c0bd5
Pablichu <pablo200016@gmail.com>
2024-11-25 20:16:53 +0100

    Definiendo namespaces

--- p3/scripts/startup.sh
+++ p3/scripts/startup.sh
@@ -2,7 +2,9 @@
 
 # Run ArgoCD
 k3d cluster create develop
+kubectl apply -f ../conf/ArgoNspace.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl apply -f ../conf/*.yaml
+kubectl apply -f ../conf/ArgoConf.yaml
 
-
+# Define dev namespace
+kubectl apply -f ../conf/DevNspace.yaml
