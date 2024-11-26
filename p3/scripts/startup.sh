2d7115fe704c125c87b830ba732d5a34f9f06b09
Pablichu <pablo200016@gmail.com>
2024-11-26 21:01:03 +0100

    Waiter for secret

--- p3/scripts/startup.sh
+++ p3/scripts/startup.sh
@@ -9,6 +9,32 @@ kubectl apply -f ../conf/ArgoConf.yaml
 # Define dev namespace
 kubectl apply -f ../conf/DevNspace.yaml
 
-echo 'ArgoCD password:'
-kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
+
+# Wait for ArgoCD to be ready and show password
+TIMEOUT=360  # Timeout in seconds
+INTERVAL=5  # Check every 5 seconds
+
+echo "Waiting for pod 'argocd-server' in namespace 'argocd' to be in 'Running' state..."
+
+# Initialize timer
+START_TIME=$(date +%s)
+
+while true; do
+  # Get the pod status
+  STATUS=$(kubectl get pod -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath="{.items[0].status.phase}" 2>/dev/null)
+  
+  # Check if the pod is in Running state
+  if [[ "$STATUS" == "Running" ]]; then
+    echo "Pod 'argocd-server' is running."
+    echo "ArgoCD password:"
+    kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
+    echo ""
+    exit 0
+  fi
+
+  echo $STATUS
+  # Wait before checking again
+  sleep $INTERVAL
+done
+
 
