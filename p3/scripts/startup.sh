719e5e717ff09af0973b9bc7ae3b2ae7bcf24f69
Pablichu <pablo200016@gmail.com>
2024-11-26 20:33:10 +0100

    Exposing app & argocd

--- p3/scripts/startup.sh
+++ p3/scripts/startup.sh
@@ -1,10 +1,14 @@
 #/bin/sh
 
 # Run ArgoCD
-k3d cluster create develop -p "8080:443@loadbalancer" #-p "
+k3d cluster create develop -p 80:80@loadbalancer -p 8888:8888@loadbalancer
 kubectl apply -f ../conf/ArgoNspace.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -f ../conf/ArgoConf.yaml
 
 # Define dev namespace
 kubectl apply -f ../conf/DevNspace.yaml
+
+echo 'ArgoCD password:'
+kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
+
