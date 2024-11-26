9e87e4ef3208edec23ace3a46b4e2ab602fbd982
Pablichu <pablo200016@gmail.com>
2024-11-26 19:41:25 +0100

    Exposing service

--- p3/scripts/startup.sh
+++ p3/scripts/startup.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 # Run ArgoCD
-k3d cluster create develop
+k3d cluster create develop -p "8080:443@loadbalancer" #-p "
 kubectl apply -f ../conf/ArgoNspace.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -f ../conf/ArgoConf.yaml
