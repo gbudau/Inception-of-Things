128e1a4280791634148b6373a8d91a2058bd0712
Pablichu <pablo200016@gmail.com>
2024-11-30 18:22:15 +0100

    Reorganizando configuraciones

--- p3/scripts/startup.sh
+++ p3/scripts/startup.sh
@@ -1,14 +1,14 @@
 #/bin/sh
 
-# Run ArgoCD
+# Starts the cluster
 k3d cluster create develop -p 8080:80@loadbalancer -p 8888:80@loadbalancer
-kubectl apply -f ../conf/ArgoNspace.yaml
-kubectl apply -k ../conf
-kubectl apply -f ../conf/ArgoConf.yaml
-kubectl apply -f ../conf/ArgoIngress.yaml
 
-# Define dev namespace
-kubectl apply -f ../conf/DevNspace.yaml
+# Deploys Namespaces
+kubectl apply -k ../conf/Namespaces
+sleep 3
+
+# Deploys ArgoCD app & conf
+kubectl apply -k ../conf/ArgoCD
 
 
 # Wait for ArgoCD to be ready and show password
@@ -17,6 +17,8 @@ INTERVAL=10  # Check every 5 seconds
 echo "Waiting for the ArgoCD Admin secret to be created... (approx. 3 minute)"
 
 while true; do
+  sleep $INTERVAL
+
   # Get the pod status
   STATUS=$(kubectl get pod -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath="{.items[0].status.phase}" 2>/dev/null)
   
@@ -35,8 +37,6 @@ while true; do
   fi
 
   echo $STATUS
-  # Wait before checking again
-  sleep $INTERVAL
 done
 
 
