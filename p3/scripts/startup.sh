828f33e24b69d738712dbd51600722f784fc390b
Pablichu <pablo200016@gmail.com>
2024-12-01 21:44:24 +0100

    Para que no salga ningun error irrelevante

--- p3/scripts/startup.sh
+++ p3/scripts/startup.sh
@@ -9,6 +9,8 @@ sleep 3
 
 # Deploys ArgoCD app & conf
 kubectl apply -k ../conf/ArgoCD
+kubectl apply -f ../conf/ArgoConf.yaml
+kubectl apply -f ../conf/ArgoIngress.yaml
 
 
 # Wait for ArgoCD to be ready and show password
